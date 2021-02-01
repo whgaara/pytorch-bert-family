34fb29f587d53e5f353d8b0924add8bc67aed5df
695509060@qq.com <695509060@qq.com>
2021-02-01 15:52:00 +0800

    up

--- tasks/ner/step1_ner_parse.py
+++ tasks/ner/step1_ner_parse.py
@@ -3,32 +3,52 @@
 # linux环境使用
 # import sys
 # sys.path.append('根目录')
+import random
 
 from ner_config import *
+from bert.common.tokenizers import Tokenizer
 
 
-def gen_item(file_path):
-    words = []
-    label = []
-    word_classes = []
-    word_nums = []
-
-    with open(file_path, 'r', encoding='utf-8') as f:
-        for word_couple in f:
-            if word_couple:
-                word, label = tuple(word_couple.strip().split(' '))
-                x = 1
-
-
-
+tokenizer = Tokenizer(VocabPath)
 
 
 def parse(src_path, train_path, eval_path, c2n_path):
+    wordlabel2num = {
+        'ptzf': 0
+    }
     f_train = open(train_path, 'w', encoding='utf-8')
     f_eval = open(eval_path, 'w', encoding='utf-8')
+    all_items = []
 
     for file in os.listdir(src_path):
-        gen_item(os.path.join(src_path, file))
+        words = []
+        wordnums = []
+        labels = []
+        labelnums = []
+        with open(os.path.join(src_path, file), 'r', encoding='utf-8') as f:
+            for word_couple in f:
+                if word_couple:
+                    word, label = tuple(word_couple.strip().split(' '))
+                    words.append(word)
+                    wordnums.append(tokenizer.token_to_id(word))
+                    if label == 'O':
+                        label = 'ptzf'
+                    if label == 'ptzf':
+                        labels.append(label)
+                        labelnums.append(wordlabel2num[label])
+                    else:
+                        labels.append(label)
+                        if label not in wordlabel2num:
+                            wordlabel2num[label] = len(wordlabel2num)
+                        labelnums.append(wordlabel2num[label])
+                    all_items.append([words, wordnums, labels, labelnums])
+    random.shuffle(all_items)
+    train_line = int(len(all_items) * TrainRate)
+    train_items = all_items[:train_line]
+    eval_items = all_items[train_line:]
+
+    # 写入到文件中
+
 
 
 if __name__ == '__main__':
