6bcb98c4f3cc019924dd3ac43809cb532e730e0e
695509060@qq.com <695509060@qq.com>
2021-02-04 14:33:28 +0800

    完成ner数据读取模块

--- bert/data/ner_dataset.py
+++ bert/data/ner_dataset.py
@@ -38,11 +38,8 @@ class NerDataSet(Dataset):
                 batch_item[2] = batch_item[2] + ['ptzf'] * (group_max_len - len(batch_item[2]))
                 batch_item[3] = batch_item[3] + [0] * (group_max_len - len(batch_item[3]))
                 tmp = {
-                    # 'input_tokens': batch_item[0],
                     'input_tokens_id': batch_item[1],
-                    # 'input_tokens_label': batch_item[2],
                     'input_tokens_label_id': batch_item[3],
-
                 }
                 tmp = {k: torch.tensor(v, dtype=torch.long) for k, v in tmp.items()}
                 dict_group.append(tmp)
@@ -55,52 +52,40 @@ class NerDataSet(Dataset):
         return self.tar_lines[item]
 
 
-# class NerEvalSet(Dataset):
-#     def __init__(self):
-#         self.tokenizer = Tokenizer(VocabPath)
-#         self.src_lines = []
-#         self.tar_lines = []
-#
-#         # 载入类别和编号的映射表
-#         with open(Class2NumFile, 'rb') as f:
-#             self.class_to_num = pickle.load(f)
-#
-#         # 读取测试数据
-#         with open(NerEvalPath, 'r', encoding='utf-8') as f:
-#             for line in f:
-#                 if line:
-#                     line = line.strip()
-#                     self.src_lines.append(line)
-#
-#         for line in self.src_lines:
-#             items = line.split(',')
-#             input_tokens, input_tokens_id, input_tokens_class, input_tokens_class_id = items
-#             if not input_tokens:
-#                 continue
-#             input_tokens_id = [int(x) for x in input_tokens_id.split(' ')]
-#             input_tokens_class_id = [int(x) for x in input_tokens_class_id.split(' ')]
-#             segment_ids = []
-#             for x in input_tokens_class_id:
-#                 if x:
-#                     segment_ids.append(1)
-#                 else:
-#                     segment_ids.append(0)
-#             tmp = {
-#                 'input_tokens_id': input_tokens_id,
-#                 'input_tokens_class_id': input_tokens_class_id,
-#                 'segment_ids': segment_ids
-#             }
-#             tmp = {k: torch.tensor(v, dtype=torch.long) for k, v in tmp.items()}
-#             self.tar_lines.append(tmp)
-#
-#     def __len__(self):
-#         return len(self.tar_lines)
-#
-#     def __getitem__(self, item):
-#         return self.tar_lines[item]
+class NerEvalSet(Dataset):
+    def __init__(self):
+        self.tokenizer = Tokenizer(VocabPath)
+        self.tar_lines = []
+
+        # 读取训练数据
+        with open(TrainPath, 'r', encoding='utf-8') as f:
+            for line in f:
+                if line:
+                    line = line.strip()
+                    items = line.split('\t')
+                    input_tokens, input_tokens_id, input_tokens_label, input_tokens_label_id = items
+                    if not input_tokens:
+                        continue
+                    input_tokens_id = [int(x) for x in input_tokens_id.split(' ')]
+                    input_tokens_label_id = [int(x) for x in input_tokens_label_id.split(' ')]
+                    tmp = {
+                        'input_tokens_id': input_tokens_id,
+                        'input_tokens_label_id': input_tokens_label_id,
+                    }
+                    tmp = {k: torch.tensor(v, dtype=torch.long) for k, v in tmp.items()}
+                    self.tar_lines.append(tmp)
+
+    def __len__(self):
+        return len(self.tar_lines)
+
+    def __getitem__(self, item):
+        return self.tar_lines[item]
 
 
 if __name__ == '__main__':
-    dataset = NerDataSet()
-    for i, data in enumerate(dataset):
+    # dataset = NerDataSet()
+    # for i, data in enumerate(dataset):
+    #     print(data)
+    evalset = NerEvalSet()
+    for i, data in enumerate(evalset):
         print(data)
